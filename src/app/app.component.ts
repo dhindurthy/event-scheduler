02d24fb0c9c5620035fbf41b8890be6b994c41a4
dhindurthy <dhirajindurthy@gmail.com>
2017-02-06 20:25:39 -0600

    Webpack added to support scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`,
-  //styleUrls:"_app.component.scss"
-  styleUrls: ['./styles.scss'],
+  //styleUrls:["./_app.component.scss"]
+  styleUrls: ['./styles.scss']
 })
 export class AppComponent  { name = 'Angular'; }
