aa453dc51f4774fcb7b0210eaf58fd71a6fae84b
dhindurthy <dhirajindurthy@gmail.com>
2017-02-09 13:59:13 -0600

    Updated the structure to seperate html/scss/ts files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1 class="heading-one">Hello whats up are you insane {{name}}</h1>`,
-  styleUrls: ['./styles.scss']
-  //styleUrls: ['./_app.component.scss']
+  moduleId: module.id,
+  templateUrl: 'app.component.html',
+  styleUrls: ['../styles.scss']
 })
 export class AppComponent  { name = 'Angular'; }
